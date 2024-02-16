72125408ff1854c4531bb0d9540fb5fae7988ccb
Sodiq Ganiyu <gsolaniyi@gmail.com>
2024-02-17 00:12:57 +0100

    im getting a hand of it

--- src/App.js
+++ src/App.js
@@ -3,12 +3,35 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(newItem) {
+    setItems([...items, newItem]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+    console.log(items);
+  }
+
   return (
-    <div className="App">
+    <div className="app">
       <Header />
-      <Form />
-      <ParkingList />
-      <Stats />
+      <Form onAddItem={handleAddItems} />
+      <ParkingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+      />
+      <Stats items={items} />
     </div>
   );
 }
@@ -17,7 +40,7 @@ function Header() {
   return <h1>🌴 Go Far Away 💼</h1>;
 }
 
-function Form() {
+function Form({ onAddItem }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
@@ -27,14 +50,14 @@ function Form() {
     if (!description) return;
     const newItem = { id: Date.now(), description, quantity, packed: false };
 
-    console.log(newItem);
+    onAddItem(newItem);
 
     setDescription("");
     setQuantity(1);
   }
 
   return (
-    <form className="form" onSubmit={handleSubmit}>
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for the trip?</h3>
       <select
         value={quantity}
@@ -50,13 +73,89 @@ function Form() {
         type="text"
         placeholder="text..."
         value={description}
-        onChange={(e) => setDescription(e.target.value)}
+        onChange={(e) => setDescription(e.target.value)} // straight up (actual) arrow function
+        // 'e' in this case is an argument sent into the function .. old way equivalent below
+        // function (e) {
+        //   return setDescription(e.target.value)  // retunr is hiden in arrow function
+        // }
       />
       <button>ADD</button>
     </form>
   );
 }
-function ParkingList() {}
-function Stats() {}
+function ParkingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
+  return (
+    <div className="list">
+      <ul>
+        {sortedItems.map((item) => (
+          <Item
+            key={item.id}
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+          />
+        ))}
+      </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value={"packed"}>sort by packed</option>
+          <option value={"description"}>sort by description</option>
+          <option value={"input"}>sort by input order</option>
+        </select>
+        <button>Clear List</button>
+      </div>
+    </div>
+  );
+}
+function Item({ item, onDeleteItem, onToggleItem }) {
+  return (
+    <li>
+      <input type="checkbox" onChange={() => onToggleItem(item.id)} />
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
+      {/* we already have the actual function above, this is just used to call
+      the actual function 
+      it could have been written as onDeleteItem(item.id) 
+      but the extra () => is to control when it is ran 
+      */}
+    </li>
+  );
+}
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <em className="stats">Start adding some items to your parking list</em>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed === true).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+  return (
+    <div className="stats">
+      <em>
+        You have {numItems} item in your list, and you already packed{" "}
+        {numPacked} ({percentage}%){" "}
+      </em>
+    </div>
+  );
+}
 
 export default App;
